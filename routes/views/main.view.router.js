a3ec1f04e4f3c0a20195e53239428584b5a17b13
LebedevD174 <Lebedevdima174@yandex.ru>
2024-03-21 15:57:42 +0300

    create select for city

--- routes/views/main.view.router.js
+++ routes/views/main.view.router.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const MainPage = require('../../components/pages/MainPage');
 const { Card } = require('../../db/models');
+const cities = ['Санкт-Петербург', 'Москва', 'Челябинск', 'Трехгорный', 'Либерти-Сити', 'Вайтран', 'Новиград', 'Монштадт', 'Валь\'шара']
 
 router.get('/', async (req, res) => {
   try {
     const { user } = res.app.locals;
     const cards = await Card.findAll();
     res.send(
-      res.renderComponent(MainPage, { user, title: 'Главная страница', cards })
+      res.renderComponent(MainPage, { user, title: 'Главная страница', cards, cities, classForm: 'mainCityFormFilter' })
     );
   } catch ({ message }) {
     res.status(200).json({ error: message });
