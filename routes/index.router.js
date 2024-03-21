bc8a3ba3946aa270f4022c3163c2eb2335e1dcd4
donikorch <DoniKorch48@yandex.ru>
2024-03-21 15:58:55 +0300

    basket release 0

--- routes/index.router.js
+++ routes/index.router.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 const mainViewRouter = require('./views/main.view.router');
 const logRegViewRouter = require('./views/logreg.view.router');
 const logRegApiRouter = require('./api/logreg.api.router');
+const basketViewRouter = require('./views/basket.view.router');
 
 //api
 router.use('/api/user', logRegApiRouter);
@@ -11,6 +12,7 @@ router.use('/api/user', logRegApiRouter);
 //router views
 router.use('/', mainViewRouter);
 router.use('/user', logRegViewRouter);
+router.use('/basket', basketViewRouter);
 
 // route api
 
