8785547bc7d5f17ee89a0f8e0f19cad8471aa2e6
MaryFBakeer <mariabefrank@gmail.com>
2024-03-22 10:26:59 +0300

    re12

--- components/pages/MainPage.jsx
+++ components/pages/MainPage.jsx
@@ -7,7 +7,10 @@ function MainPage({ title, user, cards, cities, classForm }) {
   return (
     <Layout user={user} title={title}>
       <div className='mainPage'>
+  
+
         <SearchForm cities={cities} classForm={classForm}/>
+
         <ContainerCard cards={cards} user={user}/>
       </div>
     </Layout>
