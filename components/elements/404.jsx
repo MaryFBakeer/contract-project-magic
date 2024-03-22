72b4e32ceea462f05f941372f15b6cbe60a1d307
MaryFBakeer <mariabefrank@gmail.com>
2024-03-22 15:53:06 +0300

    e

--- components/elements/404.jsx
+++ components/elements/404.jsx
@@ -1,14 +1,32 @@
 const React = require('react');
-const Layout = require('../Layout')
+const Layout = require('../Layout');
 
-function err({user}) {
+function err({ user }) {
   return (
-    <Layout>
-<div style={{display: 'flex', justifyContent: 'center'}}>
-        404
-    </div>
+    <Layout user={user}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <div
+          className='sign'
+          style={{
+            fontSize: '82px',
+          }}
+        >
+          <span className='fast-flicker'>4</span>0
+  
+          <span className='flicker'>4</span>!<p>BRRRRRR</p>
+        </div>
+        <img
+          style={{ width: '1000px' }}
+          src='https://i.pinimg.com/originals/2a/83/01/2a8301417050b08ceeb7f37e879c81f3.gif'
+        />
+      </div>
     </Layout>
-    
   );
 }
 
